4873ad22946afe4b6e3928f3e48c00efc7cc1b8f
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2023-05-31 11:58:46 +0200

    add test unitaires

--- Actionneurs/LED/BDT_LED.py
+++ Actionneurs/LED/BDT_LED.py
@@ -5,7 +5,7 @@ import time
 # Initialisation de la LED (avec paramètre de luminosité)
 pixels = neopixel.NeoPixel(machine.Pin(1), brightness = 0.2, auto_write = False, pixel_order = neopixel.GRB)
 
-def blink ():
+while True:
     pixels.fill((0, 255, 0)) # remplir avec les variables
     pixels.show() # allumer la led en fonction de fill
     time.sleep(1)
@@ -15,3 +15,4 @@ def blink ():
     pixels.fill((0, 0, 255))
     pixels.show()
     time.sleep(1)
+    
