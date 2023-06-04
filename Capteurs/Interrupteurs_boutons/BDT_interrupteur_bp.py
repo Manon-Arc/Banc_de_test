8559435cfc38aef1b0f0b6420a784715e87c8a33
baptistegdb <93204061+baptistegdb@users.noreply.github.com>
2023-06-05 00:33:18 +0200

    Delete Capteurs directory

--- Capteurs/Interrupteurs_boutons/BDT_interrupteur_bp.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-
-bp_1 = Pin(26, Pin.IN)    # broche 26 en entrée
-
-while True:
-    if bp_1.value() == 1:
-        print("bp pressed")
-    else:
-        print("bp not pressed")
